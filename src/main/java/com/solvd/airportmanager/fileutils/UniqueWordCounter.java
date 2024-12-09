e6aa4f624ac62435eea935d05b0dd76e511aa22b
Anastasiyab92 <nastiashashok@gmail.com>
2024-12-09 10:41:33 +0300

    Edited UniqueWordCounter

--- src/main/java/com/solvd/airportmanager/fileutils/UniqueWordCounter.java
+++ src/main/java/com/solvd/airportmanager/fileutils/UniqueWordCounter.java
@@ -16,19 +16,21 @@ public class UniqueWordCounter {
 
     private static final Logger LOGGER = LogManager.getLogger(UniqueWordCounter.class);
 
-    public static void main(String[] args) {
+    public static void countUniqueWords(File inputFile, File outputFile) {
         try {
-            File inputFile = new File("src/main/resources/Nabokov_Lolita.txt");
             String content = FileUtils.readFileToString(inputFile, StandardCharsets.UTF_8);
-
             String[] words = StringUtils.split(content, " \n\r\t.,;:!?()[]\"'");
             Set<String> uniqueWords = new HashSet<>(Arrays.asList(words));
-
-            File outputFile = new File("src/main/resources/unique_words_count.txt");
             FileUtils.writeStringToFile(outputFile, "Count unique words: " + uniqueWords.size(), StandardCharsets.UTF_8);
             LOGGER.info("Count unique words: {}", uniqueWords.size());
         } catch (IOException e) {
             LOGGER.error("Error while working with files: ", e);
         }
+    }
+
+    public static void main(String[] args) {
+        File inputFile = new File("src/main/resources/Nabokov_Lolita.txt");
+        File outputFile = new File("src/main/resources/unique_words_count.txt");
+        UniqueWordCounter.countUniqueWords(inputFile, outputFile);
     }
 }
